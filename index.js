f614c131fd0b204aa80a22944932dccc2ee4dcfa
Raziur-Rahman <mdraziurrahman051@gmail.com>
2023-11-08 23:24:38 +0600

    Codes For geting rooms data bassed on ID

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -33,6 +33,12 @@ async function run() {
         const cursor = roomsCollection.find();
         const result = await cursor.toArray();
         res.send(result);
+    })
+    app.get("/rooms/:id", async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await roomsCollection.findOne(query);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
